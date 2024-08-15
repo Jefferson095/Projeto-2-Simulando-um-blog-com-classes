36487ea6741c329aad31010979e05f256c8655c0
Jefferson🔥 <160180992+Jefferson095@users.noreply.github.com>
2024-08-15 15:51:30 -0300

    Update Author.js

--- Author.js
+++ Author.js
@@ -1,1 +1,33 @@
 
+Author.js
+
+const Post = require('./Post')
+
+
+
+
+class Author{
+
+    constructor(author){
+
+        this.author = author
+
+        this.posts = []
+
+    }
+
+
+
+
+    addPost(newPost){
+
+        this.posts.push(newPost)
+
+    }
+
+}
+
+
+
+
+module.exports = Author
