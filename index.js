cf9d2ab7955fc07209383e9382ca108179afc3b9
Jefferson🔥 <160180992+Jefferson095@users.noreply.github.com>
2024-08-15 15:45:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
+index.js
 
+
+
+
+const Comment = require('./Comment')
+
+const Author = require('./Author')
+
+const Post = require('./Post')
+
+
+
+
+const author1 = new Author(
+
+    'Jefferson',
+
+)
+
+
+
+
+const post1 = new Post(
+
+    'Título do post de Jefferson ', 'Conteúdo do post e Jefferson',
+
+    author1
+
+)
+
+
+
+
+
+
+
+
+const comment1 = new Comment(
+
+    'Paulo', 'Que legal!'
+
+)
+
+
+const comment2 = new Comment(
+
+    'Jonas', 'Você de novo por aqui.'
+
+)
+
+author1.addPost(post1)
+
+post1.addComment(comment1)
+
+post1.addComment(comment2)
+
+console.log(author1)
+
+console.log(post1)
