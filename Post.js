8ab2b2baec19811483ae603877bcaeaf3dc8da4b
Jefferson🔥 <160180992+Jefferson095@users.noreply.github.com>
2024-08-22 10:33:06 -0300

    Update Post.js

--- Post.js
+++ Post.js
@@ -1,43 +1,16 @@
+const Comment = require('./Comment')
 
-Post
+class Post {
+  constructor(title, content, author) {
+    this.title = title
+    this.content = content
+    this.author = author
+    this.comment = []
+  }
 
-const Author = require('./Author')
-
-
-
-
-class Post{
-
-    constructor(title,content,author){
-
-        this.title = title
-
-        this.content = content
-
-        this.author = author
-
-        this.comment = []
-
-    }
-
-
-
-
-    addComment(comment){
-
-        this.comment.push(comment)
-
-        return this.comment
-
-    }
-
+  addComments(author, content) {
+    this.comment.push(new Comment(author, content))
+  }
 }
-
-
-
-
-
-
-
 
 module.exports = Post
